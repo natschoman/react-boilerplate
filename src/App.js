0f601c76ea64f04fec92028be78442653a21b60d
natschoman <natschoman@gmail.com>
2019-09-29 21:17:15 +0200

    add babel plugin-proposal-class-properties

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
 class App extends React.Component {
+  state = {
+    count: 0
+  }
+
   render(){
-    return <h1>Hello world!</h1>
+    const {count} = this.state;
+    return (
+      <div>
+        <h1>Hello world!</h1>
+        <h2>Count: {count}</h2>
+        <button onClick={() => this.setState({count: count + 1})}>+</button>
+        <button onClick={() => this.setState({count: count - 1})}>-</button>
+      </div>
+    )
   }
 }
 
