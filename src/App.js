4ce7a46ba139c8a8da82ae37043f16174fbf63e9
natschoman <natschoman@gmail.com>
2019-09-30 21:45:08 +0200

    add prettier and prettier-quick for formatting last edited files

--- src/App.js
+++ src/App.js
@@ -1,38 +1,36 @@
-import React from 'react';
-import {hot} from 'react-hot-loader';
+import React from "react";
+import { hot } from "react-hot-loader";
 
-const Warning = React.lazy(() => import('./Warning'));
+const Warning = React.lazy(() => import("./Warning"));
 
 class App extends React.Component {
   state = {
     count: 0
-  }
+  };
 
   increment = () => {
-    this.setState({count: this.state.count + 1});
-  }
+    this.setState({ count: this.state.count + 1 });
+  };
 
   decrement = () => {
-    this.setState({count: this.state.count - 1});
-  }
+    this.setState({ count: this.state.count - 1 });
+  };
 
-  render(){
-    const {count} = this.state;
+  render() {
+    const { count } = this.state;
     return (
       <div>
         <h1>Hello world!</h1>
-        <h2 className={count > 10 ? 'warning' : null}>
-          Count: {count}
-        </h2>
+        <h2 className={count > 10 ? "warning" : null}>Count: {count}</h2>
         <button onClick={this.increment}>+</button>
         <button onClick={this.decrement}>-</button>
-        {count > 10 ? 
+        {count > 10 ? (
           <React.Suspense fallback={null}>
-            <Warning /> 
+            <Warning />
           </React.Suspense>
-        : null}
+        ) : null}
       </div>
-    )
+    );
   }
 }
 
