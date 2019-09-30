d17419aa695cfc7558e55296fa1fefe76b455757
natschoman <natschoman@gmail.com>
2019-09-30 15:35:12 +0200

    npm script calls another with flag

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
     const {count} = this.state;
     return (
       <div>
-        <h1>Hello world!!!</h1>
+        <h1>Hello world!</h1>
         <h2 className={count > 10 ? 'warning' : null}>
           Count: {count}
         </h2>
